a3a39dfa584339a812d2292982223e4b508f066d
SamsonSoans <ssamsonsoans@gmail.com>
2021-10-03 21:31:03 +0530

    [Ng 12] Upgrading to Angular 12. Test Pending

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
@@ -13,8 +12,7 @@ import { ProductPageComponent } from './product-page/product-page.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
